43256565869d7553a522a7ba8d7d7e355014be48
mrultima <mrultima@users.noreply.github.com>
2021-08-27 14:16:04 +0300

    rooms detayları eklendi

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,31 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { from, timer } from 'rxjs';
-import { groupBy, mergeMap, toArray } from 'rxjs/operators';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, from, Subject, timer } from 'rxjs';
+import { groupBy, mergeMap, takeUntil, toArray } from 'rxjs/operators';
 import { ApiService } from '../api.service';
-import { Room } from '../types';
+import { Room, Rooms } from '../types';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
-export class RoomsComponent implements OnInit {
+export class RoomsComponent implements OnInit,OnDestroy,AfterViewInit {
 
   rooms: Room[]=[];
   roomsByGroup:any[] = [];
-  constructor(public apiService: ApiService) { }
+  rooms$ = new BehaviorSubject<Rooms | null>(null);
+
+  isDestroyed = new Subject();
+  constructor(public apiService: ApiService) {
+   
+    
+   }
+  async ngAfterViewInit(){
+    
+    
+  }
+  ngOnDestroy(): void {
+    this.isDestroyed.next();
+    this.isDestroyed.complete(); 
+  }
 
   async ngOnInit(){
-    this.apiService.rooms$.subscribe((info) => {this.rooms = info
+    this.apiService.rooms$.subscribe((info) => {
     this._groupBy(info);
-    console.log("grop",this.roomsByGroup);
     })
     await timer(500).toPromise();
-  
+    this.apiService.rooms$.pipe(takeUntil(this.isDestroyed)).subscribe(info=>{
+      if(info){
+        this.rooms$.next(info)
+        this.rooms$.getValue()?.forEach(room => this.rooms.push(room))
+        console.log(this.rooms$.value)
+        for(let i= 0 ; i < this.rooms.length ; i++)
+        if(this.rooms[i]){
+          if(this.rooms[i]?.RoomImageURL == null){
+            this.rooms[i].RoomImageURL = "https://erspublic.blob.core.windows.net/test/1721248f-f039-5867-86fc-2e5b0a257000.jpg"
+          }
+        }        
+      }
+    })
+    this.apiService.onSearch();  
   }
 
   search(){
-    console.log(this.rooms)
+    
   }
 
   _groupBy(data:Room[]){
